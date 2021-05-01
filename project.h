d5e5fd5386fef4ba0c01d7de58be4bd630e983f9
JM <22000789@handong.edu>
2021-05-01 20:54:50 +0900

    update project.h

--- project.h
+++ project.h
@@ -10,3 +10,6 @@ int addInstrument(Instrument *i); //제품을 추가하는 함수
 void showInstrument(Instrument *i); //제품 하나를 보여주는 함수
 int updateInstrument(Instrument *i); //수정함수
 int deleteInstrument(Instrument *i); //삭제함수
+void listInstrument(Instrument *i), int count; //전체 리스트 함수
+int selectInstrument(Instrument *i, int count); //수정할 번호
+void saveData(Instrument *i); // 데이터 저장
