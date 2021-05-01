7e0c95e0b1f50ec83a64783b93cdf856bc23c2b9
JM <22000789@handong.edu>
2021-05-01 21:09:26 +0900

    add main.c

---
+++ main.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <string.h>
+#include "project.h"
+
+int main(){
+	Instrument ilist[100];
+	int menu, count =0;
+	int index = 0;
+
+	while(1){
+                int x = 0;
+                menu = SelectNumber();
+                if(menu == 0){
+                        break;
+                }
+                if(menu == 1){
+			if(count > 0)
+				listInsrument(i, index);
+                      	else
+				continue;
+                }
+                if(menu == 2){
+                        count += addInstrument(&ilist[index++]);
+                        x++;
+                }
+                else if(menu == 3){
+                        int no = SelectInstrument(ilist, index);
+                        if(no == 0){
+                                printf("=>취소되었습니다.\n");
+                                continue;
+                        }
+                        updateInstrument(&ilist[no-1]);
+                }
+                else if(menu == 4){
+                        int no = selectInstrument(ilist, index);
+                        if(no == 0){
+                                printf("=> 취소되었습니다.\n");
+                        }
+                        else{
+                                int delete;
+                                delete = deleteinstrument(&ilist[no-1]);
+                                if(delete == 1){
+                                        printf("=> 삭제되었습니다.\n");
+                                        count--;
+                                        }
+                                else
+                                        printf("=> 취소되었습니다.\n");
+                        }
+                        printf("\n");
+                }
+                else if(menu == 5){
+                        saveData(flist, index);
+                }
+		printf("\n");
+        }
+        printf("=> 종료\n");
+        return 0;
+}
+
